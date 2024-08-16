534b8fd91255e31fd79ae198caca68f1917c42e4
Abhishek Sharma <107794611+abhishek-sharma-1993@users.noreply.github.com>
2024-08-16 14:54:26 +0530

    Time: 7 ms (82.7%), Space: 65 MB (21.52%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
@@ -6,14 +6,13 @@ class Solution {
     
         for(int i=1; i<arrays.size(); i++){
                 int N = arrays.get(i).size();
+                int first = arrays.get(i).get(0);
+                int last = arrays.get(i).get(N-1);
 
-                int diff1 = Math.abs(arrays.get(i).get(N-1) - min); 
-                int diff2 = Math.abs(max - arrays.get(i).get(0)); 
-
-                min = Math.min(min, arrays.get(i).get(0));
-                max = Math.max(max, arrays.get(i).get(N-1));
-
-                ans = Math.max(ans, Math.max(diff1, diff2));
+                ans = Math.max(ans, Math.max(Math.abs(last - min), Math.abs(max - first)));
+                
+                min = Math.min(min, first);
+                max = Math.max(max, last);
         }
 
         return ans;
