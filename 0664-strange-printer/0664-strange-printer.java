52531cfed259b50f26e42f33a64720cbb03926a9
Abhishek Sharma <107794611+abhishek-sharma-1993@users.noreply.github.com>
2024-08-21 17:38:42 +0530

    Time: 15 ms (55.51%), Space: 43.4 MB (29.8%) - LeetHub

---
+++ 0664-strange-printer/0664-strange-printer.java
@@ -0,0 +1,53 @@
+class Solution {
+    String str;
+    int[][] dp;
+    public int strangePrinter(String s) {
+        this.str = removeDuplicate(s);
+        int n = str.length();
+        this.dp = new int[n][n];
+        return minTurns(0,n-1);
+    }
+
+    private int minTurns(int start, int end){
+        if(start>end) return 0;
+        if(dp[start][end]!=0)return dp[start][end];
+
+        int minTurns = 1 + minTurns(start+1, end);
+        
+        for(int k = start+1; k<=end; k++){
+            if(str.charAt(k) == str.charAt(start)){
+                int minTurnMatch = minTurns(start,k-1)+minTurns(k+1, end);
+                minTurns = Math.min(minTurns, minTurnMatch);
+            }    
+        }
+        
+        dp[start][end] = minTurns;
+        return minTurns;
+    }
+
+    private String removeDuplicate(String s){
+        StringBuilder str = new StringBuilder();
+
+        Deque<Character> stack = new ArrayDeque<>();
+        int i = 0;
+        while(i<s.length()){
+            char ch = s.charAt(i);
+            while(!stack.isEmpty() && stack.getLast()==ch)
+                stack.removeLast();
+            
+            stack.addLast(ch);
+            i++;
+        }
+
+        while(!stack.isEmpty())
+            str.append(stack.removeFirst());
+        
+        return str.toString();
+    }
+}
+
+//abbbba : a=2, b=4
+/*
+ * 
+ *
+*/
