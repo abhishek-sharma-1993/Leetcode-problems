b3b28d38a94c01dd98ee3914a72752d8789e3594
Abhishek Sharma <107794611+abhishek-sharma-1993@users.noreply.github.com>
2024-08-24 18:31:39 +0530

    Time: 7 ms (6.84%), Space: 44.6 MB (5.43%) - LeetHub

--- 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.java
+++ 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.java
@@ -1,37 +1,56 @@
 class Solution {
     public String nearestPalindromic(String n) {
-        int len = n.length();
-        int mid = len%2==0 ? len/2-1 : len/2; 
-        long firstHalf = Long.parseLong(n.substring(0,mid+1));
-
-        List<Long> possible = new ArrayList<>();
-        possible.add(helper(firstHalf, len%2==0));
-        possible.add(helper(firstHalf+1, len%2==0));
-        possible.add(helper(firstHalf-1, len%2==0));
-        possible.add((long) Math.pow(10, len-1)-1); //999
-        possible.add((long) Math.pow(10,len)+1); //10001
-
-        long res = 0, num = Long.parseLong(n), diff = Long.MAX_VALUE;
-        for(long cand : possible){
-            if(cand == num) continue;
-            else if(Math.abs(cand-num)<diff){
-                diff = Math.abs(cand-num);
-                res = cand;
-            }else if(Math.abs(cand-num)==diff){
-                res = Math.min(res, cand);
-            }
-        }
-        return String.valueOf(res);
-
+        long num = Long.parseLong(n);
+        long prev = prevPalindrom(num);
+        long next = nextPalindrom(num);
+        if(Math.abs(num-prev)>=Math.abs(next-num))
+            return String.valueOf(next);
+        
+        return String.valueOf(prev);
     }
 
-    private long helper(long left, boolean even){
-        long res = left;
-        if(!even) left = left/10;
-        while(left>0){
-            res = res*10 + (left%10);
-            left /= 10;
+    private long helper(long num){
+        String palin = String.valueOf(num);
+        int n = palin.length();
+        int l=(n-1)/2, r=n/2;
+        char[] seq = palin.toCharArray();
+        while(l>=0){
+            seq[r++] = seq[l--];
         }
-        return res;
+        return Long.parseLong(new String(seq));
+    }
+
+    private long nextPalindrom(long num){
+        long l=0, r=num;
+        long ans = Long.MIN_VALUE;
+        while(l<=r){
+            long mid = l + (r-l)/2;
+            long palin = helper(mid);
+            if(palin < num){
+                ans = palin;
+                l = mid+1;
+            }else{
+                r = mid-1;
+            }
+        }
+
+        return ans;
+    }
+
+    private long prevPalindrom(long num){
+        long l=num, r=Long.MAX_VALUE;
+        long ans = 0;
+        while(l<=r){
+            long mid = l + (r-l)/2;
+            long palin = helper(mid);
+
+            if(palin > num){
+                ans = palin;
+                r = mid-1;
+            }else{
+                l = mid+1;
+            }
+        }
+        return ans;
     }
 }
