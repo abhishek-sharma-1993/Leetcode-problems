144f4122f867988eec636acdb6ef944aa1d59b20
Abhishek Sharma <107794611+abhishek-sharma-1993@users.noreply.github.com>
2024-08-31 13:48:31 +0530

    Time: 35 ms (39.5%), Space: 58.6 MB (11.57%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.java
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.java
@@ -1,54 +1,44 @@
 class Solution {
-    public double maxProbability(int n, int[][] edges, double[] succProb, 
-                                    int start_node, int end_node) {
-        
-        PriorityQueue<Pair> maxheap = new PriorityQueue<>((a,b)-> Double.compare(b.dist,a.dist));
-        Map<Integer, List<Pair>> graph = new HashMap<>();
-
+    public double maxProbability(int n, int[][] edges, double[] succProb,
+                 int start_node, int end_node) {
+        List<Pair>[] graph = new List[n];
         for(int i=0; i<n; i++){
-            graph.put(i, new ArrayList<>());
+            graph[i] = new ArrayList<>();
         }
 
         for(int i=0; i<edges.length; i++){
-            graph.get(edges[i][0]).add(new Pair(edges[i][1], succProb[i]) );
-            graph.get(edges[i][1]).add(new Pair(edges[i][0], succProb[i]) );
+            graph[edges[i][0]].add( new Pair(edges[i][1],succProb[i]) );
+            graph[edges[i][1]].add( new Pair(edges[i][0],succProb[i]) );
         }
-
-        for(Pair nei : graph.get(start_node)){
-            maxheap.add(nei);
-        }
-
-        double ans = 1;
         double[] maxProb = new double[n];
+        Queue<Pair> heap = new PriorityQueue<>((a,b)->Double.compare(b.prob, a.prob));
+        heap.add(new Pair(start_node, 1d));
         maxProb[start_node] = 1d;
-
-        while(!maxheap.isEmpty()){
-            Pair pair = maxheap.remove();
+        while(!heap.isEmpty()){
+            Pair pair = heap.remove();
             int node = pair.node;
-            double prob = pair.dist;
-            if(node == end_node) return prob;
+            double prob = pair.prob;
 
-            for(Pair nxt : graph.get(node)){
-                double nxtProb = nxt.dist;
-                int nxtNode = nxt.node;
-
-                if(prob*nxtProb > maxProb[nxtNode]){
-                    maxProb[nxtNode] = prob*nxtProb;
-                    maxheap.add(new Pair(nxtNode, maxProb[nxtNode]));
+            for(Pair next: graph[node]){
+                int nnode = next.node;
+                double nprob = next.prob * prob;
+                if(maxProb[nnode]<nprob){
+                    maxProb[nnode] = nprob;
+                    heap.add(new Pair(nnode, nprob));
                 }
             }
         }
 
-        return 0;
+        return maxProb[end_node];
     }
 }
 
 class Pair{
     int node;
-    double dist;
+    double prob;
 
-    Pair(int node, double dist){
+    Pair(int node, double prob){
         this.node = node;
-        this.dist = dist;
+        this.prob = prob;
     }
 }
