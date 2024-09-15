5dda16108da4c0bc2192937bae831b955a58e751
Abhishek Sharma <107794611+abhishek-sharma-1993@users.noreply.github.com>
2024-09-15 16:33:08 +0530

    Time: 42 ms (58.36%), Space: 45.1 MB (74.73%) - LeetHub

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.java
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.java
@@ -1,18 +1,18 @@
 class Solution {
     public int findTheLongestSubstring(String s) {
-        int[] charMap = new int[26];
-        charMap['a'-'a'] = 1<<0;
-        charMap['e'-'a'] = 1<<1;
-        charMap['i'-'a'] = 1<<2;
-        charMap['o'-'a'] = 1<<3;
-        charMap['u'-'a'] = 1<<4;
-
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0,-1);
         int ans = 0, bitmask = 0;
 
         for(int i=0; i<s.length(); i++){
-            bitmask ^= charMap[s.charAt(i)-'a'];
+            char ch = s.charAt(i);
+            switch(ch){
+                case 'a' : bitmask ^= (1<<0);break;
+                case 'e' : bitmask ^= (1<<1);break;
+                case 'i' : bitmask ^= (1<<2);break;
+                case 'o' : bitmask ^= (1<<3);break;
+                case 'u' : bitmask ^= (1<<4);break;
+            }
 
             if(map.containsKey(bitmask))
                 ans = Math.max(ans, i-map.get(bitmask));
