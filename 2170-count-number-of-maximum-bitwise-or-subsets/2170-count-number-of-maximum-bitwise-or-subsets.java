0c4dcc3653cafce10d2ce879229cd51657fe11c9
Abhishek Sharma <107794611+abhishek-sharma-1993@users.noreply.github.com>
2024-10-18 18:46:14 +0530

    Time: 8 ms (86%), Space: 40.9 MB (80.41%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.java
@@ -2,35 +2,25 @@ class Solution {
     int n;
     int[] nums;
     int maxOr;
-    List<Integer> list;
+    int ans;
     public int countMaxOrSubsets(int[] nums) {
         this.n = nums.length;
         this.nums = nums;
         this.maxOr = 0;
-        this.list = new ArrayList<>();
-        findSubset(0, new ArrayList<>());
+        this.ans = 0;
+        for(int num: nums) maxOr |= num;
 
-        long ans = list.stream().filter(n -> n== maxOr).count();
+        findSubset(0, 0);
+
         return (int)ans;
     }
 
-    private void findSubset(int index, List<Integer> subset){
+    private void findSubset(int index, int currOr){
         if(index == n) {
-            int OR = 0;
-            for(int num: subset){
-                OR = OR | num;
-            }
-
-            maxOr = Math.max(maxOr, OR);
-            list.add(OR);
+            if(currOr == maxOr) ans++;
             return;
         }
-
-        subset.add(nums[index]);
-        findSubset(index+1, subset);
-        subset.remove(subset.size()-1);
-        findSubset(index+1, subset);
-
-    
+        findSubset(index+1, currOr | nums[index]);
+        findSubset(index+1, currOr);
     }
 }
