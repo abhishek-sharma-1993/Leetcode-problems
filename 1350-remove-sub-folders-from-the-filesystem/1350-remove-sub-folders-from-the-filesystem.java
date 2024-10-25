5c72ec84766b1dce458a3fee525910be03e8a957
Abhishek Sharma <107794611+abhishek-sharma-1993@users.noreply.github.com>
2024-10-25 14:16:19 +0530

    Time: 90 ms (23.67%), Space: 69.3 MB (13.47%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.java
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.java
@@ -1,18 +1,53 @@
 class Solution {
     public List<String> removeSubfolders(String[] folder) {
-        Arrays.sort(folder);
+       TrieNode root = new TrieNode();
+
+       for(String str : folder){
+            TrieNode currNode = root;
+            String[] names = str.split("/");
+
+            for(String name: names){
+                if(name.isEmpty()) continue;
+
+                if(!currNode.child.containsKey(name)){
+                    currNode.child.put(name, new TrieNode());
+                }
+                currNode = currNode.child.get(name);
+            }
+            currNode.isEnd = true;
+       }
 
         List<String> ans = new ArrayList<>();
-        ans.add(folder[0]);
+        for(String str: folder){
+            TrieNode currNode = root;
+            String[] names = str.split("/");
 
-        for(int i=1; i<folder.length; i++){
-            String tmp = ans.get(ans.size()-1);
-            tmp += "/";
+            boolean isSubFolder = false;
+            for(int i=0; i<names.length; i++){
+                if(names[i].isEmpty()) continue;
 
-            if(!folder[i].startsWith(tmp)){
-                ans.add(folder[i]);
+                TrieNode nextNode = currNode.child.get(names[i]);
+
+                if(nextNode.isEnd && i != names.length-1){
+                    isSubFolder = true;
+                    break;
+                }
+                currNode = nextNode;
             }
+
+            if(!isSubFolder) ans.add(str);
         }
+
         return ans;
+    }
+}
+
+class TrieNode{
+    boolean isEnd;
+    Map<String, TrieNode> child;
+
+    TrieNode(){
+        isEnd = false;
+        child = new HashMap<>();
     }
 }
