256b6a217c4a63dfff66b52cb2a56c82a6b243f5
Abhishek Sharma <107794611+abhishek-sharma-1993@users.noreply.github.com>
2024-11-16 14:32:37 +0530

    Time: 1 ms (100%), Space: 45.2 MB (81.91%) - LeetHub

--- 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.java
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.java
@@ -2,23 +2,28 @@ class Solution {
     public int[] resultsArray(int[] nums, int k) {
         int n = nums.length;
         int[] ans = new int[n-k+1];
+
+        int nonConsecutive = 0;
+        for(int i=1; i<k; i++){
+            if(nums[i]!=nums[i-1]+1) 
+                nonConsecutive++;
+        }
         int left=0,right=k-1;
         int i=0;
         while(right<n){
-            ans[i++] = validate(nums, left, right);
+            ans[i++] = nonConsecutive == 0 ? nums[right] : -1;
+
+            if(left<n-1 && nums[left+1] != nums[left]+1){
+                nonConsecutive--;
+            }
+
+            if(right<n-1 && nums[right+1] != nums[right]+1){
+                nonConsecutive++;
+            }
+
             left++;
             right++;
         }
         return ans;
-    }
-    private int validate(int[] nums, int i, int j){
-        int max = nums[i];
-        i++;
-        while(i<=j){
-            max = Math.max(max, nums[i]);
-            if(nums[i-1]>=nums[i] || (nums[i]-nums[i-1]) > 1) return -1;
-            i++;
-        }
-        return max;
     }
 }
