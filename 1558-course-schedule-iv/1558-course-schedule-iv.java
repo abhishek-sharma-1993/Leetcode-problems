a41032560ec3f88f628dabe1a8b7630502dcfeb1
Abhishek Sharma <107794611+abhishek-sharma-1993@users.noreply.github.com>
2025-01-27 21:08:23 +0530

    Time: 149 ms (21.03%), Space: 50.4 MB (14.27%) - LeetHub

--- 1558-course-schedule-iv/1558-course-schedule-iv.java
+++ 1558-course-schedule-iv/1558-course-schedule-iv.java
@@ -11,25 +11,38 @@ class Solution {
         for(int[] edge : prerequisites){
             graph[edge[0]].add(edge[1]);
         }
-
+        boolean[][] isReachable = new boolean[numCourses][numCourses];
         List<Boolean> ans = new ArrayList<>();
+
         for(int[] query: queries){
-            boolean res = dfs(new boolean[numCourses], query[0], query[1]);
+            boolean res = true;
+
+            if(!isReachable[query[0]][query[1]]){
+                res = dfs(new boolean[numCourses], 
+                    query[0], query[1], isReachable[query[0]]);
+            }
+                
             ans.add(res);
         }
         return ans;
     }
     
-    private boolean dfs(boolean[] visited, int node, int target){
+    private boolean dfs(
+        boolean[] visited, 
+        int node, 
+        int target,
+        boolean[] isReachable){
+
         if(node == target) 
             return true;
 
         visited[node] = true;
+        isReachable[node] = true;
         boolean ans = false;
 
         for(int nei: graph[node]){
             if(!visited[nei]) 
-                ans |= dfs(visited, nei, target);
+                ans |= dfs(visited, nei, target, isReachable);
         }
 
         return ans;
