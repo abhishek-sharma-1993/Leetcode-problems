39866235eee6920dbd97d5c7a392b157d5a9809b
Abhishek Sharma <107794611+abhishek-sharma-1993@users.noreply.github.com>
2025-04-02 11:05:30 +0530

    Time: 11 ms (35.86%), Space: 124 MB (33.33%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
@@ -1,17 +1,19 @@
 class Solution {
+    long[] dp;
     public long mostPoints(int[][] questions) {
-        long[] dp = new long[questions.length];
-        for (int i = questions.length - 1; i >= 0; i--) {
-            int index = i + questions[i][1] + 1;
-            if (index < questions.length) {
-                dp[i] = dp[index] + questions[i][0];
-            } else {
-                dp[i] = questions[i][0];
-            }
-            if (i < questions.length - 1) {
-                dp[i] = Math.max(dp[i + 1], dp[i]);
-            }
-        }
-        return dp[0];
+        this.dp = new long[questions.length+1];
+        Arrays.fill(dp, -1l);
+       return helper(0, questions);
+    }
+
+    private long helper(int idx, int[][] ques){
+        if(ques.length <= idx) return 0;
+        if(dp[idx] != -1) return dp[idx];
+
+        long take = helper(idx+ques[idx][1]+1, ques) + ques[idx][0];
+        long dont = helper(idx+1, ques);
+
+        dp[idx] = Math.max(take, dont);
+        return dp[idx];
     }
 }
